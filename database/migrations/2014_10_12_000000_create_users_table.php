0d598bea15a3db73ce89ffeb40c5a1529db0605f
Kunal Varma <kunalvarma05@gmail.com>
2016-03-17 07:33:02 +0530

    Add additional fields to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('id')->index();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('picture')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
