03cf5a77e1dad971dd3697e1d7caf2cdf72f3cc3
Kunal Varma <kunalvarma05@gmail.com>
2016-03-21 06:03:26 +0530

    Create accounts table migration

---
+++ database/migrations/2016_03_17_020814_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid')->nullable();
+            $table->string('name')->nullable();
+            $table->text('access_token')->nullable();
+            $table->string('picture')->nullable();
+            $table->integer('provider_id')->unsigned();
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('accounts');
+    }
+}
