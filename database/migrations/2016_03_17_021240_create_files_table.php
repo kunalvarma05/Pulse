dcbc46635fd5ae72ab8f94f6c7841436f2f92d12
Kunal Varma <kunalvarma05@gmail.com>
2016-03-21 06:03:43 +0530

    Create files table migration

---
+++ database/migrations/2016_03_17_021240_create_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('key')->nullable();
+            $table->boolean('is_encrypted')->default(0);
+            $table->integer('account_id')->unsigned();
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('files');
+    }
+}
