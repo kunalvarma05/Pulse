a46eba2d7585b7f74f97d0297efc4bd941fd262f
Kunal Varma <kunalvarma05@gmail.com>
2016-04-01 23:34:41 +0530

    Notifications dropdown

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,16 +3,36 @@
         <input class="form-control navbar-search-input" type="text" placeholder="Search">
     </form>
     <ul class="nav navbar-nav pull-right">
-        <li class="nav-item dropdown notifications-dropdown">
+        <li class="nav-item dropdown notifications-menu">
             <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
                 <span class="label label-primary notification-count animated zoomIn">2</span>
                 <i class="fa fa-bell"></i>
             </a>
-            <div class="dropdown-menu dropdown-menu-right">
-                <h6 class="dropdown-header">Notifications</h6>
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-menu dropdown-menu-right notifications-dropdown">
+                <div class="dropdown-header">
+                    Notifications
+                    <div class="pull-right">
+                        <a href="#" class="label ">View All</a>
+                    </div>
+                </div>
+                <a href="#" class="dropdown-item media notification-item">
+                    <span class="media-left">
+                        <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
+                    </span>
+                    <div class="media-body">
+                        <b>hello.txt</b> was transferred to <b>Kunal's Dropdbox</b> from <b>Kunal's OneDrive</b>.
+                        <span class="timestamp">2 mins ago</span>
+                    </div>
+                </a>
+                <a href="#" class="dropdown-item media notification-item">
+                    <span class="media-left">
+                        <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
+                    </span>
+                    <div class="media-body">
+                        <b>hello.txt</b> was created.
+                        <span class="timestamp">5 mins ago</span>
+                    </div>
+                </a>
             </div>
         </li>
         <img src="https://en.gravatar.com/userimage/24319273/c93bac54fc0d0105ad9bfed905aee458.jpg?size=100" class="navbar-account-image">
