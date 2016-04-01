5f78323d6463d8e4bbc1cd6acd7c87332a3953be
Kunal Varma <kunalvarma05@gmail.com>
2016-04-01 20:45:28 +0530

    Sidemenu and navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-fixed-top navbar-light navbar-main">
+    <form class="navbar-search-form">
+        <input class="form-control navbar-search-input" type="text" placeholder="Search">
+    </form>
+    <ul class="nav navbar-nav pull-right">
+        <li class="nav-item dropdown notifications-dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                <span class="label label-primary notification-count animated zoomIn">2</span>
+                <i class="fa fa-bell"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <h6 class="dropdown-header">Notifications</h6>
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        <img src="https://en.gravatar.com/userimage/24319273/c93bac54fc0d0105ad9bfed905aee458.jpg?size=100" class="navbar-account-image">
+        <li class="nav-item dropdown account-dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                Kunal Varma <span class="caret"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Settings</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Log out</a>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/partials/sidemenu.blade.php
@@ -0,0 +1,33 @@
+<nav class="sidemenu">
+    <a href="{{ url('/') }}" class="sidemenu-logo">
+        <img src="{{ asset('images/logo-white.png') }}" alt="pulse logo">
+    </a>
+
+    <ul class="nav sidemenu-user-accounts">
+        <li>
+        <a class="sidemenu-user-account" href="#" style="animation-delay: .5s;">
+                <img src="https://randomuser.me/api/portraits/med/men/14.jpg">
+            </a>
+        </li>
+        <li>
+        <a class="sidemenu-user-account" href="#" style="animation-delay: .6s;">
+                <img src="https://randomuser.me/api/portraits/med/men/19.jpg">
+            </a>
+        </li>
+        <li>
+        <a class="sidemenu-user-account" href="#" style="animation-delay: .7s;">
+                <img src="https://randomuser.me/api/portraits/med/men/9.jpg">
+            </a>
+        </li>
+        <li>
+        <a class="sidemenu-user-account" href="#" style="animation-delay: .8s;">
+                <img src="https://randomuser.me/api/portraits/med/men/18.jpg">
+            </a>
+        </li>
+        <li>
+        <a class="sidemenu-add-button" href="#">
+                <span class="fa fa-plus"></span>
+            </a>
+        </li>
+    </ul>
+</nav>
