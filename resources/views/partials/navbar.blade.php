82f30ba230b378884f13a4c856968170a758886d
Kunal Varma <kunalvarma05@gmail.com>
2016-04-02 12:00:46 +0530

    Notification Dropdown Partial

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,35 +5,12 @@
     <ul class="nav navbar-nav pull-right">
         <li class="nav-item dropdown notifications-menu">
             <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-                <span class="label label-primary notification-count animated zoomIn">2</span>
+                <span class="label label-primary notification-count animated zoomIn">3</span>
                 <i class="fa fa-bell"></i>
             </a>
-            <div class="dropdown-menu dropdown-menu-right notifications-dropdown">
-                <div class="dropdown-header">
-                    Notifications
-                    <div class="pull-right">
-                        <a href="#" class="label ">View All</a>
-                    </div>
-                </div>
-                <a href="#" class="dropdown-item media notification-item">
-                    <span class="media-left">
-                        <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
-                    </span>
-                    <div class="media-body">
-                        <b>hello.txt</b> was transferred to <b>Kunal's Dropdbox</b> from <b>Kunal's OneDrive</b>.
-                        <span class="timestamp">2 mins ago</span>
-                    </div>
-                </a>
-                <a href="#" class="dropdown-item media notification-item">
-                    <span class="media-left">
-                        <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
-                    </span>
-                    <div class="media-body">
-                        <b>hello.txt</b> was created.
-                        <span class="timestamp">5 mins ago</span>
-                    </div>
-                </a>
-            </div>
+
+            @include('partials.notification-dropdown')
+
         </li>
         <img src="https://en.gravatar.com/userimage/24319273/c93bac54fc0d0105ad9bfed905aee458.jpg?size=100" class="navbar-account-image">
         <li class="nav-item dropdown account-dropdown">
---
+++ resources/views/partials/notification-dropdown.blade.php
@@ -0,0 +1,37 @@
+<div class="dropdown-menu dropdown-menu-right notifications-dropdown">
+    <div class="dropdown-header">
+        Notifications
+        <div class="pull-right">
+            <a href="#" class="label ">View All</a>
+        </div>
+    </div>
+    <div class="notification-items">
+        <a href="#" class="dropdown-item media notification-item">
+            <span class="media-left">
+                <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
+            </span>
+            <div class="media-body">
+                <b>hello.txt</b> was encrypted.
+                <span class="timestamp">1 min ago</span>
+            </div>
+        </a>
+        <a href="#" class="dropdown-item media notification-item">
+            <span class="media-left">
+                <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
+            </span>
+            <div class="media-body">
+                <b>hello.txt</b> was transferred to <b>Kunal's Dropbox</b> from <b>Kunal's OneDrive</b>.
+                <span class="timestamp">2 mins ago</span>
+            </div>
+        </a>
+        <a href="#" class="dropdown-item media notification-item">
+            <span class="media-left">
+                <img class="media-object" src="https://randomuser.me/api/portraits/med/men/9.jpg" alt="img">
+            </span>
+            <div class="media-body">
+                <b>hello.txt</b> was created.
+                <span class="timestamp">5 mins ago</span>
+            </div>
+        </a>
+    </div>
+</div>
