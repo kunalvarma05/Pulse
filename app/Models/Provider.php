21069d3b4fac6d4075b58d34a4038d098adb3f24
Kunal Varma <kunalvarma05@gmail.com>
2016-04-06 17:26:52 +0530

    Provider Model

---
+++ app/Models/Provider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pulse;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Provider extends Model
+{
+    /**
+     * Table
+     */
+    protected $table = "providers";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    'title', 'description', 'link', 'picture',
+    ];
+
+}
