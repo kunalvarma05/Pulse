8fb1f1e14973533b70bef4eff54fcb9851126d20
Kunal Varma <kunalvarma05@gmail.com>
2016-04-07 13:40:40 +0530

    Welcome email view

---
+++ resources/views/emails/user/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Welcome to Pulse</title>
+</head>
+<body>
+  <h1>Welcome to Pulse</h1>
+  <p>
+    Hey {{$name}}, welcome to Pulse.
+    <a href="{{url('/')}}">Log in</a>
+  </p>
+</body>
+</html>
