2c5fe8b4ab91802d028a46906beab68405559ab1
Kunal Varma <kunalvarma05@gmail.com>
2016-04-07 15:28:08 +0530

    Delete User Command Handler

---
+++ app/Bus/Handlers/Commands/User/DeleteUserCommandHandler.php
@@ -0,0 +1,29 @@
+<?php
+namespace Pulse\Bus\Handlers\Commands\User;
+
+use Pulse\Models\User;
+use Pulse\Events\User\UserWasDeletedEvent;
+use Pulse\Bus\Commands\User\DeleteUserCommand;
+
+class DeleteUserCommandHandler
+{
+
+    /**
+     * Handle the Delete User Command
+     *
+     * @param  Pulse\Bus\Commands\User\DeleteUserCommand $command
+     * @return Pulse\Models\User
+     */
+    public function handle(DeleteUserCommand $command)
+    {
+        //User
+        $user = $command->user;
+
+        //Fire the UserWasDeletedEvent
+        event(new UserWasDeletedEvent($user));
+
+        //Delete the User
+        $user->delete();
+    }
+
+}
