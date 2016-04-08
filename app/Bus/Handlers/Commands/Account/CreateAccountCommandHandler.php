a2a539b4636420f7dbf318f64e21713806a92e99
Kunal Varma <kunalvarma05@gmail.com>
2016-04-08 13:40:05 +0530

    Create Account Command handler

---
+++ app/Bus/Handlers/Commands/Account/CreateAccountCommandHandler.php
@@ -0,0 +1,36 @@
+<?php
+namespace Pulse\Bus\Handlers\Commands\Account;
+
+use Pulse\Events\Account\AccountWasCreatedEvent;
+use Pulse\Bus\Commands\Account\CreateAccountCommand;
+
+class CreateAccountCommandHandler
+{
+
+    /**
+     * Handle the Create Account Command
+     *
+     * @param  Pulse\Bus\Commands\Account\CreateAccountCommand $command
+     * @return Pulse\Models\Account
+     */
+    public function handle(CreateAccountCommand $command)
+    {
+        //Create Account
+        $account = $command
+        ->user
+        ->accounts()
+        ->create([
+            'provider_id' => $command->provider->id,
+            'name' => $command->name,
+            'uid' => $command->uid,
+            'access_token' => $command->access_token,
+            ]);
+
+        //Fire the AccountWasCreatedEvent
+        event(new AccountWasCreatedEvent($account));
+
+        //Return the created account
+        return $account;
+    }
+
+}
