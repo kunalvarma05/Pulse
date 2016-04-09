f43b1ddef471c531e5153869ab6601c5ce7a6530
Kunal Varma <kunalvarma05@gmail.com>
2016-04-09 14:51:58 +0530

    Use Resolver while creating account

--- app/Api/Controllers/AccountsController.php
+++ app/Api/Controllers/AccountsController.php
@@ -5,30 +5,41 @@ use Auth;
 use Pulse\Models\Account;
 use Pulse\Models\Provider;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Pulse\Services\Authorization\ResolverInterface;
 use Pulse\Api\Requests\Account\CreateAccountRequest;
 use Pulse\Bus\Commands\Account\CreateAccountCommand;
 
 class AccountsController extends BaseController
 {
-    public function create(CreateAccountRequest $request)
+
+    /**
+     * Create new Account
+     * @param  CreateAccountRequest $request
+     * @param  ResolverInterface    $resolver
+     * @return Response
+     */
+    public function create(CreateAccountRequest $request, ResolverInterface $resolver)
     {
         //Current User
         $user = Auth::user();
         //Provider
         $provider = Provider::find($request->get('provider'));
 
-        //Dispatch CreateAccessTokenCommand
-        //@todo Generate Access Token from auth code
-        $access_token = $request->get('code');
+        //Resolve Auth Provider
+        $authProvider = $resolver->resolve(strtolower($provider->alias));
+
+        //@todo Dispatch CreateAccessTokenCommand
+        $access_token = $authProvider->getAccessToken($request->get('code'), $request->get('state'));
+
         //@todo
-        $uid = "xyz1234";
+        $uid = mt_rand();
 
         //Dispatch CreateAccountCommand
         $account = dispatch(new CreateAccountCommand(
             $user,
             $provider,
             $request->get('name'),
-            $uid
+            $uid,
             $access_token
             ));
 
