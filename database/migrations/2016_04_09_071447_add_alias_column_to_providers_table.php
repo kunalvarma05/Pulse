112ef470a4aded47c7d592193a6676f0b2aa823c
Kunal Varma <kunalvarma05@gmail.com>
2016-04-09 14:06:46 +0530

    Add alias column to providers table migration

---
+++ database/migrations/2016_04_09_071447_add_alias_column_to_providers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAliasColumnToProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            $table->string('alias', 30)->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            $table->dropColumn('alias');
+        });
+    }
+}
