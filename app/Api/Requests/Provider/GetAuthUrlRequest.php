5b57126e933bf3b527bdfde8e657a079b2863a89
Kunal Varma <kunalvarma05@gmail.com>
2016-04-09 14:51:00 +0530

    Provider Get Auth URL Request

---
+++ app/Api/Requests/Provider/GetAuthUrlRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pulse\Api\Requests\Provider;
+
+use Pulse\Http\Requests\Request;
+
+class GetAuthUrlRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'provider' => 'bail|required|exists:providers,id'
+        ];
+    }
+}
