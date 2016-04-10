f21a3e2afdb0eda874b03db96d6bcdf720bcf036
Kunal Varma <kunalvarma05@gmail.com>
2016-04-10 19:29:54 +0530

    Get Default User Picture

--- app/Utils/Helpers.php
+++ app/Utils/Helpers.php
@@ -3,14 +3,19 @@ namespace Pulse\Utils;
 
 class Helpers {
 
-    public static function defaultAccountPicture($provider_alias)
+    public static function defaultAccountPicture()
     {
-        return asset("images/providers/{$provider_alias}.png");
+        return asset("images/user.png");
     }
 
     public static function defaultProviderPicture($provider_alias)
     {
         return asset("images/providers/{$provider_alias}.png");
+    }
+
+    public static function defaultUserPicture()
+    {
+        return asset("images/user.png");
     }
 
 }
