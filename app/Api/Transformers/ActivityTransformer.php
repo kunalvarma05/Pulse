c3d8e7cfca443d85d966136dd9aaec512265aa89
Kunal Varma <kunalvarma05@gmail.com>
2016-04-11 18:02:59 +0530

    Activity Transformer

---
+++ app/Api/Transformers/ActivityTransformer.php
@@ -0,0 +1,59 @@
+<?php
+namespace Pulse\Api\Transformers;
+
+use Pulse\Utils\Helpers;
+use Pulse\Models\Action;
+use Pulse\Models\Transfer;
+use Pulse\Models\Activity;
+use League\Fractal\TransformerAbstract;
+
+class ActivityTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        'user',
+        'transaction'
+    ];
+
+    public function transform(Activity $activity)
+    {
+        return [
+            "id" => (int) $activity->id,
+            "status" => $activity->status,
+            "user_id" => (int) $activity->user_id,
+            "transaction_type" => $activity->transaction_type,
+            "transaction_id" => (int) $activity->transaction_id,
+            "created_at" => $activity->created_at ? $activity->created_at->diffForHumans() : "",
+            "updated_at" => $activity->updated_at ? $activity->updated_at->diffForHumans() : ""
+        ];
+    }
+
+    /**
+     * Include User
+     *
+     * @return League\Fractal\Resource\Item
+     */
+    public function includeUser(Activity $activity)
+    {
+        return $this->item($activity->user, new UserTransformer);
+    }
+
+    /**
+     * Include Transaction
+     *
+     * @return League\Fractal\Resource\Item
+     */
+    public function includeTransaction(Activity $activity)
+    {
+        if($activity->transaction_type === Action::class) {
+            return $this->item($activity->transaction, new ActionTransformer);
+        }
+
+        return $this->item($activity->transaction, new TransferTransformer);
+    }
+
+}
