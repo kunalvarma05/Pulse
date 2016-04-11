1b5f1c67f0d5a3159615c78d651f9cca205c8403
Kunal Varma <kunalvarma05@gmail.com>
2016-04-12 01:17:13 +0530

    OneDrive OAuth Scopes

--- config/onedrive.php
+++ config/onedrive.php
@@ -3,5 +3,6 @@
 return [
 'client_id'          => env('MICROSOFT_CLIENT_ID'),
 'client_secret'      => env('MICROSOFT_CLIENT_SECRET'),
-'callback_url'       => env('MICROSOFT_CALLBACK_URL')
+'callback_url'       => env('MICROSOFT_CALLBACK_URL'),
+'scope'              => ['wl.basic', 'wl.signin', 'wl.offline_access', 'onedrive.readwrite']
 ];
