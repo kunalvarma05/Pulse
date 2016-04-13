83211f1f8236ad1fab6cd0dcab3ab536bb425bc1
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 01:03:02 +0530

    Authorization Adapter Interface

---
+++ app/Services/Authorization/Adapters/AdapterInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pulse\Services\Authorization\Adapters;
+
+use Pulse\Services\Authorization\AuthorizationInterface;
+
+interface AdapterInterface extends AuthorizationInterface
+{
+
+}
