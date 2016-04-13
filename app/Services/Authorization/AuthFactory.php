c85668cc36c952b6dd09b8728ef4df5d344b6ebb
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 01:07:52 +0530

    Authorization Factory

---
+++ app/Services/Authorization/AuthFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace Pulse\Services\Authorization;
+
+use Pulse\Services\Authorization\Adapters\AdapterFactory;
+
+class AuthFactory implements AuthFactoryInterface {
+
+    /**
+     * Create a Authorization Service with
+     * Adapter of the specified provider
+     * @param  string $provider Adapter Provider
+     * @return AuthorizationInterface
+     */
+    public static function create($provider)
+    {
+        //Resolve Auth Adapter
+        $authAdapter = AdapterFactory::create($provider);
+
+        //Authorization Service
+        return new Authorization($authAdapter);
+    }
+
+}
