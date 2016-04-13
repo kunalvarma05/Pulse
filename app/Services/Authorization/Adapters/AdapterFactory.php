24f5f162a7c18aee363b4fee80640b542ee6117e
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 01:07:17 +0530

    Authorization Adapter Factory

---
+++ app/Services/Authorization/Adapters/AdapterFactory.php
@@ -0,0 +1,97 @@
+<?php
+namespace Pulse\Services\Authorization\Adapters;
+
+use \Google_Client;
+use Dropbox\AppInfo;
+use Dropbox\WebAuth;
+use GuzzleHttp\Client as Guzzle;
+use Stevenmaguire\OAuth2\Client\Provider\Microsoft;
+use Pulse\Services\Authorization\Adapters\Drive\AuthAdapter as DriveAdapter;
+use Pulse\Services\Authorization\Adapters\Dropbox\AuthAdapter as DropboxAdapter;
+use Pulse\Services\Authorization\Adapters\OneDrive\AuthAdapter as OneDriveAdapter;
+
+class AdapterFactory implements AdapterFactoryInterface
+{
+
+    /**
+     * Create Adapter
+     * @param  string $adapter
+     * @return AdapterInterface
+     */
+    public static function create($adapter)
+    {
+        switch ($adapter) {
+            case 'dropbox':
+                return self::createDropboxAdapter();
+                break;
+
+            case 'drive':
+                return self::createDriveAdapter();
+                break;
+
+            case 'onedrive':
+                return self::createOneDriveAdapter();
+                break;
+
+            default:
+                throw new \Exception("Please specify a valid Adapter");
+                break;
+        }
+    }
+
+    /**
+     * Create Dropbox Adapter
+     * @return AuthAdapter
+     */
+    protected static function createDropboxAdapter()
+    {
+        $appInfo = AppInfo::loadFromJson([
+                    "key" => config("dropbox.app"),
+                    "secret" => config("dropbox.secret")
+                ]);
+
+        $sessionStore = app('Pulse\Services\Authorization\Adapters\Dropbox\DropboxCsrfTokenStoreInterface');
+
+        $webAuth = new WebAuth($appInfo, "pulseapp", config("dropbox.callback_url"), $sessionStore, "en");
+
+        return new DropboxAdapter($webAuth);
+    }
+
+    /**
+     * Create Google Drive Adapter
+     * @return AuthAdapter
+     */
+    protected static function createDriveAdapter()
+    {
+        $client = new Google_Client();
+        $client->setClientId(config("drive.client_id"));
+        $client->setClientSecret(config("drive.client_secret"));
+        $client->setRedirectUri(config("drive.callback_url"));
+        $client->setAccessType('offline');
+        $client->setScopes(config("drive.scopes"));
+
+        $sessionStore = app('Illuminate\Session\Store');
+
+        return new DriveAdapter($client, $sessionStore);
+    }
+
+    /**
+     * Create OneDrive Adapter
+     * @return AuthAdapter
+     */
+    protected static function createOneDriveAdapter()
+    {
+        $provider = new Microsoft([
+            'clientId'          => config('onedrive.client_id'),
+            'clientSecret'      => config('onedrive.client_secret'),
+            'redirectUri'       => config('onedrive.callback_url')
+            ]);
+
+        $guzzleClient = new Guzzle;
+
+        $sessionStore = app('Illuminate\Session\Store');
+
+        return new OneDriveAdapter($provider, $guzzleClient, $sessionStore);
+    }
+
+}
---
+++ app/Services/Authorization/AuthFactoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pulse\Services\Authorization;
+
+interface AuthFactoryInterface {
+
+    /**
+     * Create a Authorization Service with
+     * Adapter of the specified provider
+     * @param  string $provider Adapter Provider
+     * @return AuthorizationInterface
+     */
+    public static function create($provider);
+
+}
