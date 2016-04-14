e1f5c7b0a5352523d52d572fcf849d0affe48ac2
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 17:28:44 +0530

    Identity Factory

---
+++ app/Services/Identity/IdentityFactory.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pulse\Services\Identity;
+
+use Pulse\Services\Identity\Adapters\AdapterFactory;
+
+class IdentityFactory implements IdentityFactoryInterface
+{
+
+    /**
+     * Create an Identity Service the adapter
+     * of the given identity provider.
+     * @param  string $provider     Identity Provider
+     * @param  string $access_token Access Token
+     * @return Pulse\Services\Identity\IdentityInterface
+     */
+    public static function create($provider, $access_token)
+    {
+        //Create Identity Adapter
+        $adapter = AdapterFactory::create($provider, $access_token);
+
+        //Make Identity Service with the Adapter
+        return new Identity($adapter);
+    }
+
+}
