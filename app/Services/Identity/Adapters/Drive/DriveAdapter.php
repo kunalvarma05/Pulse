8670349b666fc992b99bc5a9453ec00bd1cae3b1
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 17:28:14 +0530

    Identity Service Google Drive Adapter

---
+++ app/Services/Identity/Adapters/Drive/DriveAdapter.php
@@ -0,0 +1,82 @@
+<?php
+namespace Pulse\Services\Identity\Adapters\Drive;
+
+use Google_Client;
+use Google_Exception;
+use Google_Service_Plus;
+use Google_Service_Plus_Person;
+use Pulse\Services\Identity\Account\AccountInterface;
+use Pulse\Services\Identity\Adapters\AdapterInterface;
+
+class DriveAdapter implements AdapterInterface
+{
+
+    /**
+     * Google Plus Service
+     * @var Google_Service_Plus
+     */
+    private $service;
+
+    /**
+     * Account Info
+     * @var Pulse\Services\Identity\Account\AccountInterface;
+     */
+    private $accountInfo;
+
+    /**
+     * Constructor
+     * @param Google_Service_Plus $service
+     */
+    public function __construct(Google_Service_Plus $service, AccountInterface $accountInfo)
+    {
+        $this->service = $service;
+        $this->accountInfo = $accountInfo;
+    }
+
+    /**
+     * Get Service
+     * @return Google_Service_Plus
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * Get Account
+     * @param  string $account_id Account ID
+     * @return Pulse\Serivces\Identity\Account\AccountInterface
+     */
+    public function getAccount($account_id = null)
+    {
+        if(is_null($account_id)) {
+            $account_id = "me";
+        }
+
+        try {
+            $account = $this->getService()->people->get($account_id);
+
+            $this->makeAccountInfo($account);
+
+            return $this->accountInfo;
+        } catch (Google_Exception $e) {
+            // @todo Better Google_Exception Handling
+            return false;
+        }
+
+        return false;
+    }
+
+    /**
+     * Make the Account Info Object
+     * @param  Google_Service_Plus_Person $account
+     */
+    protected function makeAccountInfo(Google_Service_Plus_Person $account)
+    {
+        $this->accountInfo->setId($account->getId());
+        $this->accountInfo->setName($account->getDisplayName());
+        $this->accountInfo->setImage($account->getImage()->getUrl());
+        $this->accountInfo->setEmail($account->getEmails()[0]->getValue());
+    }
+
+}
