1a72c436a7308282cf643e3bf3c16f1fea8b3096
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 17:22:44 +0530

    Identity Factory Interface

---
+++ app/Services/Identity/IdentityFactoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pulse\Services\Identity;
+
+interface IdentityFactoryInterface
+{
+
+    /**
+     * Create an Identity Service the adapter
+     * of the given identity provider.
+     * @param  string $provider     Identity Provider
+     * @param  string $access_token Access Token
+     * @return Pulse\Services\Identity\IdentityInterface
+     */
+    public static function create($provider, $access_token);
+
+}
