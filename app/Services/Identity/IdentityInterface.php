af37d3b358a0d45f466f107ede2fd6237cfb9c10
Kunal Varma <kunalvarma05@gmail.com>
2016-04-14 17:26:51 +0530

    DocBlock

--- app/Services/Identity/IdentityInterface.php
+++ app/Services/Identity/IdentityInterface.php
@@ -3,5 +3,10 @@ namespace Pulse\Services\Identity;
 
 interface IdentityInterface
 {
+    /**
+     * Get Account
+     * @param  string $account_id Account ID
+     * @return Pulse\Serivces\Identity\Account\AccountInterface
+     */
     public function getAccount($account_id = null);
 }
