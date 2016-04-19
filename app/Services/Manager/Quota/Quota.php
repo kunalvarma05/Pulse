aa02d46fa5d5fc255e74b0e2985e22ffebbd2919
Kunal Varma <kunalvarma05@gmail.com>
2016-04-19 16:53:58 +0530

    formatBytes as Helper

--- app/Services/Manager/Quota/Quota.php
+++ app/Services/Manager/Quota/Quota.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pulse\Services\Manager\Quota;
 
+use Pulse\Utils\Helpers;
+
 class Quota implements QuotaInterface
 {
     /**
@@ -89,14 +91,6 @@ class Quota implements QuotaInterface
      */
     protected function formatBytes($bytes, $precision = 2)
     {
-        $units = array('B', 'KB', 'MB', 'GB', 'TB');
-
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-
-        $bytes /= (1 << (10 * $pow));
-
-        return round($bytes, $precision) . ' ' . $units[$pow];
+        return Helpers::formatBytes($bytes, $precision);
     }
 }
--- app/Utils/Helpers.php
+++ app/Utils/Helpers.php
@@ -25,4 +25,23 @@ class Helpers {
         return $icon;
     }
 
+    /**
+     * Format Bytes to human readable size
+     * @param  int  $bytes     Filesize in Bytes
+     * @param  integer $precision Return value precision
+     * @return string             Human readable filesize
+     */
+    public static function formatBytes($bytes, $precision = 2)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
 }
