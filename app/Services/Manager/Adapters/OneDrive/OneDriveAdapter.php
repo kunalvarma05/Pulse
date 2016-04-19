a8a4b04196a35e571928844a8399984f1faf17d5
Kunal Varma <kunalvarma05@gmail.com>
2016-04-20 01:50:53 +0530

    Manager Service OneDrive Adapter listChildren

--- app/Services/Manager/Adapters/OneDrive/OneDriveAdapter.php
+++ app/Services/Manager/Adapters/OneDrive/OneDriveAdapter.php
@@ -2,8 +2,10 @@
 namespace Pulse\Services\Manager\Adapters\OneDrive;
 
 use Exception;
+use Pulse\Utils\Helpers;
 use League\OAuth2\Client\Token\AccessToken;
 use Kunnu\OneDrive\Client as OneDriveClient;
+use Pulse\Services\Manager\File\FileInterface;
 use Pulse\Services\Manager\Quota\QuotaInterface;
 use Pulse\Services\Manager\Adapters\AdapterInterface;
 
@@ -61,6 +63,93 @@ class OneDriveAdapter implements AdapterInterface
         }
 
         return false;
+    }
+
+    /**
+     * List Children of a given folder path or id
+     * @param  string $path Folder path or ID
+     * @param  array  $data Additional Data
+     * @return Pulse\Services\Manager\File\FileInterface
+     */
+    public function listChildren($path = null, array $data = array())
+    {
+        //Root
+        if($path === "/")
+        {
+            $path = null;
+        }
+
+        $maxResults = isset($data['maxResults']) ? $data['maxResults'] : 48;
+        $children = $this->getService()->listChildren($path, ['top' => $maxResults, 'expand' => "thumbnails(select=medium)"]);
+        $items = $children->value;
+
+        //Files not found
+        if(empty($items))
+        {
+            return false;
+        }
+
+        //Make the file list
+        $files = $this->makeFileList($items);
+
+        return $files;
+
+    }
+
+    /**
+     * Make File List
+     * @param  array $list
+     * @return Array (Pulse\Services\Manager\File\FileInterface)
+     */
+    protected function makeFileList(array $list)
+    {
+        $files = [];
+        foreach ($list as $file) {
+            $files[] = $this->makeFile($file);
+        }
+
+        return $files;
+    }
+
+    /**
+     * Make File Object
+     * @param  $file
+     * @return Pulse\Services\Manager\File\FileInterface
+     */
+    protected function makeFile($file)
+    {
+        $fileInfo = app('Pulse\Services\Manager\File\FileInterface');
+
+        $fileInfo->setId($file->id);
+
+        $fileInfo->setTitle($file->name);
+        $path = ($file->parentReference && $file->parentReference->path) ? $file->parentReference->path : $file->id;
+        $fileInfo->setPath($path);
+        $fileInfo->setModified($file->lastModifiedDateTime);
+        $fileInfo->setSize($file->size);
+
+        $fileInfo->setURL($file->webUrl);
+        $downloadUrl = isset($file->{'@content.downloadUrl'}) ? $file->{'@content.downloadUrl'} : "";
+        $fileInfo->setDownloadURL($downloadUrl);
+
+        $isFolder = isset($file->folder) ? true : false;
+        $fileInfo->setIsFolder($isFolder);
+
+        $mime = (isset($file->file) && isset($file->file->mimeType)) ? $file->file->mimeType : "";
+        $fileInfo->setMimeType($mime);
+
+        $icon = $fileInfo->isFolder() ? "folder" : $mime;
+
+        $fileInfo->setIcon(Helpers::getFileIcon($icon));
+
+        $thumbnails = $file->thumbnails;
+        $thumbnail = (isset($thumbnails[0]) && isset($thumbnails[0]->medium)) ? $thumbnails[0]->medium->url : "";
+        $fileInfo->setThumbnailURL($thumbnail);
+
+        $owner = (isset($file->createdBy) && isset($file->createdBy->user)) ? $file->createdBy->user->displayName : "";
+        $fileInfo->setOwners([$owner]);
+
+        return $fileInfo;
     }
 
     /**
