fd7918ccbd12ac5278cb49f2f665665c05dd9306
Kunal Varma <kunalvarma05@gmail.com>
2016-05-06 03:05:30 +0530

    Add scheduled_at column to transfers table

---
+++ database/migrations/2016_05_05_213231_add_scheduled_at_column_to_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddScheduledAtColumnToTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->timestamp('scheduled_at')->after('to_account_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->dropColumn('scheduled_at');
+        });
+    }
+}
