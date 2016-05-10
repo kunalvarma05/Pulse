5366509d038fba19acbc110e34e37e54eda8bb6a
Kunal Varma <kunalvarma05@gmail.com>
2016-05-11 04:19:41 +0530

    Add location column to transfers table migration

---
+++ database/migrations/2016_05_06_055141_add_location_column_to_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLocationColumnToTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->string('location')->after('account_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transfers', function (Blueprint $table) {
+            $table->dropColumn('location');
+        });
+    }
+}
